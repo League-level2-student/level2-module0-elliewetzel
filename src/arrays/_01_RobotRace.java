14a741c0999782a4d05602c037bdb0ea53a01bc0
ArielBaruch <league@WTS14-4.attlocal.net>
2019-04-05 16:58:54 -0700

    _01_RobotRace and _02_FindTheHiddenButton (IP)

--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -2,19 +2,38 @@ package arrays;
 
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class _01_RobotRace {
 	static Robot[] bots = new Robot[5];
 	static Random r = new Random();
 	static boolean race = true;
+	static int[] turns = new int[5];
 
 	// 1. make a main method
 	public static void main(String[] args) {
 		for (int i = 0; i < 5; i++) {
 			bots[i] = new Robot();
+		}for(int i = 0; i < bots.length; i++) {
+			bots[i].moveTo(140 + 230 * i, 490);
 		}
-		for (int i = 0; i < bots.length; i++) {
+		while(race) {
+			for(int i=0; i < bots.length; i++) {
+			//bots[i].turn(1);
+			bots[i].move(1);
+			bots[i].turn(1);
+			turns[i]++;
+			if(turns[i] >= 360) {
+				race = false;
+				break;
+			}
+			}
+		}
+		
+		
+		/*for (int i = 0; i < bots.length; i++) {
 			bots[i].moveTo(90 + 180 * i, 550);
 		}
 		while (race) {
@@ -22,10 +41,12 @@ public class _01_RobotRace {
 				bots[i].move(r.nextInt(50));
 				if (bots[i].getY() < 0) {
 					race = false;
+					JOptionPane.showMessageDialog(null, bots[i] + " is the Winner!!");
 					break;
 				}
 			}
-		}
+		}*/
+		
 		// 2. create an array of 5 robots.
 		// 3. use a for loop to initialize the robots.
 		// 4. make each robot start at the bottom of the screen, side by side, facing up
--- src/arrays/_02_FindTheHiddenButton.java
+++ src/arrays/_02_FindTheHiddenButton.java
@@ -20,9 +20,9 @@ public class _02_FindTheHiddenButton implements ActionListener{
 	JPanel panel;
 	
 	//1. create an array of JButtons. Don't initialize it yet.
-	
+	JButton[] buttons;
 	//2 create an int variable called hiddenButton
-	
+	int hiddenButton;
 	public static void main(String[] args) {
 		new _02_FindTheHiddenButton().start();
 	}
@@ -33,9 +33,14 @@ public class _02_FindTheHiddenButton implements ActionListener{
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		//3.  Ask the user to enter a positive number and convert it to an int
-		
+		String r = JOptionPane.showInputDialog("Please enter a positive number.");
+		Integer.parseInt(r);
 		//4. Initialize the array of JButtons to be the size of the int created in step 3
-		
+		for(int i = 0; i < buttons.length; i++) {
+			buttons[i] = new JButton(r);
+			buttons[i].addActionListener(this);
+			panel.add(buttons[i]);
+		}
 		//5. Make a for loop to iterate through the JButton array
 			//6. initialize each JButton in the array
 			//7. add the ActionListener to each JButton
